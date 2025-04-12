4467052375b09b7e3aca5e50aef7876421b6fec7
azechi <azechi.n@gmail.com>
2025-04-12 16:24:02 +0900

    とりあえずindex.htmlはキャッシュしないで開発する
    COOPヘッダーを追加するだけ

--- sw.ts
+++ sw.ts
@@ -1,22 +1,22 @@
 /// <reference lib="webworker" />
+/// <reference types="vite/client" />
 
 export type { };
 declare const self: ServiceWorkerGlobalScope;
 
-self.addEventListener("install", event =>{
-    event.waitUntil(self.caches.open("1").then(cache =>{
-        return cache.add("index.html");
-    }));
-});
-
 self.addEventListener("fetch", event => {
+
+    if (import.meta.env.MODE == "development"){
+        return;
+    }
+
     const req = event.request;
     if (req.mode != "navigate" || req.url != self.registration.scope) {
         return;
     }
 
     event.respondWith((async () => {
-        const res = (await self.caches.match("index.html"))!;
+        const res = await fetch(req);
         return new Response(res.body, {
             ...res,
             headers: new Headers([
