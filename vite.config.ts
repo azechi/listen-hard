d49ba56fa17094f380303cc81381577e0f66c956
azechi <azechi.n@gmail.com>
2025-05-27 09:09:44 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
         server: {
             allowedHosts: true,
         },
+        appType: "mpa",
         build: {
             target: "esnext",
             rollupOptions: {
